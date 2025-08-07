ec69e871e09ff78d0eac7805ab26480ae3a39b89
TheRemonDev <riyanahmed3536@gmail.com>
2025-08-07 20:39:31 +0100

    add-max-3-in-function

--- max3.js
+++ max3.js
@@ -13,3 +13,18 @@ if (rim > jim && rim > tim) {
 }
 
 // আউটপুট : রিম ইস দ্যা আনলিমিডেট বস, প্রথমে আমরা চেক করবো রিম, জিম আর টিম থেকে বড় কিনা,যদি বড় হয় তাহলে এখানেই থেমে যাবে, আর যদি বড় না হয় আমরা চেক করবো জিম, রিম আর টিম থেকে বড় কিনা, যদি সেটাও বড় না হয় তাহলে টিম সবার থেকে বড় হবে
+
+
+// Ekhn eita k Function e chalabo
+
+function maxThree(name1 , name2, name3) {
+    if (name1 > name2 && name1 > name3) {
+        return name1;
+    } else if (name2 > name1 && name2 > name3) {
+        return name2;
+    } else {
+        return name3
+    }
+}
+const maxwin = maxThree(90, 70, 80);  //কিন্তু function এর ভিতরে যা কাজ হচ্ছে, সেটা number compare করার logic।
+ console.log(maxwin);
